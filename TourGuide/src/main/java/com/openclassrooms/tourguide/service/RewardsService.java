dab1561bf1fd120085d0d68a74e17e4036f00cc8
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 19:48:25 +0200

    replace into first if condition (.filter(..).count == 0) by  noneMatch(..)

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -37,7 +37,7 @@ public class RewardsService {
 		
 		for(VisitedLocation visitedLocation : userLocations) {
 			for(Attraction attraction : attractions) {
-				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
+				if(user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName))) {
 					if(nearAttraction(visitedLocation, attraction)) {
 						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
 					}
