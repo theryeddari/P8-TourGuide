265d8a4d04b8d8e58c5bc0a58cb592c5199142d2
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 19:39:21 +0200

    resolve camelCase name of class

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideApplication.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TourguideApplication {
+public class TourGuideApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TourguideApplication.class, args);
+		SpringApplication.run(TourGuideApplication.class, args);
 	}
 
 }
