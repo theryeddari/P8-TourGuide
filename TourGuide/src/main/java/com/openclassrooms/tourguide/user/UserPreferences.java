199010c176b845344bbb9ca43f77f2efd8db6d8f
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 19:30:13 +0200

    use lombok Getter and Setter to replace get and set method

--- TourGuide/src/main/java/com/openclassrooms/tourguide/user/UserPreferences.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/user/UserPreferences.java
@@ -1,5 +1,10 @@
 package com.openclassrooms.tourguide.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserPreferences {
 	
 	private int attractionProximity = Integer.MAX_VALUE;
@@ -9,46 +14,6 @@ public class UserPreferences {
 	private int numberOfChildren = 0;
 	
 	public UserPreferences() {
-	}
-	
-	public void setAttractionProximity(int attractionProximity) {
-		this.attractionProximity = attractionProximity;
-	}
-	
-	public int getAttractionProximity() {
-		return attractionProximity;
-	}
-	
-	public int getTripDuration() {
-		return tripDuration;
-	}
-
-	public void setTripDuration(int tripDuration) {
-		this.tripDuration = tripDuration;
-	}
-
-	public int getTicketQuantity() {
-		return ticketQuantity;
-	}
-
-	public void setTicketQuantity(int ticketQuantity) {
-		this.ticketQuantity = ticketQuantity;
-	}
-	
-	public int getNumberOfAdults() {
-		return numberOfAdults;
-	}
-
-	public void setNumberOfAdults(int numberOfAdults) {
-		this.numberOfAdults = numberOfAdults;
-	}
-
-	public int getNumberOfChildren() {
-		return numberOfChildren;
-	}
-
-	public void setNumberOfChildren(int numberOfChildren) {
-		this.numberOfChildren = numberOfChildren;
 	}
 
 }
