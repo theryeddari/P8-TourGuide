92ea4ac51201a503d69b27c9d6ccc87b44e9a90f
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 19:37:04 +0200

    use lombok AllArgsConstructor to automatic constructor

--- TourGuide/src/main/java/com/openclassrooms/tourguide/user/UserReward.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/user/UserReward.java
@@ -2,22 +2,19 @@ package com.openclassrooms.tourguide.user;
 
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class UserReward {
 
 	public final VisitedLocation visitedLocation;
 	public final Attraction attraction;
 	private int rewardPoints;
-	public UserReward(VisitedLocation visitedLocation, Attraction attraction, int rewardPoints) {
-		this.visitedLocation = visitedLocation;
-		this.attraction = attraction;
-		this.rewardPoints = rewardPoints;
-	}
-	
+
 	public UserReward(VisitedLocation visitedLocation, Attraction attraction) {
 		this.visitedLocation = visitedLocation;
 		this.attraction = attraction;
