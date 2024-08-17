18289e8dd6e52f35ac88f9b6662799738e9d1ef9
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 18:49:38 +0200

    use lombok Getter and Setter to replace get and set method

--- TourGuide/src/main/java/com/openclassrooms/tourguide/user/User.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/user/User.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.UUID;
 
 import gpsUtil.location.VisitedLocation;
+import lombok.Getter;
+import lombok.Setter;
 import tripPricer.Provider;
 
+@Getter
+@Setter
 public class User {
 	private final UUID userId;
 	private final String userName;
@@ -24,48 +28,12 @@ public class User {
 		this.phoneNumber = phoneNumber;
 		this.emailAddress = emailAddress;
 	}
-	
-	public UUID getUserId() {
-		return userId;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
 
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
-	
-	public String getEmailAddress() {
-		return emailAddress;
-	}
-	
-	public void setLatestLocationTimestamp(Date latestLocationTimestamp) {
-		this.latestLocationTimestamp = latestLocationTimestamp;
-	}
-	
-	public Date getLatestLocationTimestamp() {
-		return latestLocationTimestamp;
-	}
-	
-	public void addToVisitedLocations(VisitedLocation visitedLocation) {
+    public void addToVisitedLocations(VisitedLocation visitedLocation) {
 		visitedLocations.add(visitedLocation);
 	}
-	
-	public List<VisitedLocation> getVisitedLocations() {
-		return visitedLocations;
-	}
-	
-	public void clearVisitedLocations() {
+
+    public void clearVisitedLocations() {
 		visitedLocations.clear();
 	}
 	
@@ -74,29 +42,9 @@ public class User {
 			userRewards.add(userReward);
 		}
 	}
-	
-	public List<UserReward> getUserRewards() {
-		return userRewards;
-	}
-	
-	public UserPreferences getUserPreferences() {
-		return userPreferences;
-	}
-	
-	public void setUserPreferences(UserPreferences userPreferences) {
-		this.userPreferences = userPreferences;
-	}
 
 	public VisitedLocation getLastVisitedLocation() {
 		return visitedLocations.get(visitedLocations.size() - 1);
-	}
-	
-	public void setTripDeals(List<Provider> tripDeals) {
-		this.tripDeals = tripDeals;
-	}
-	
-	public List<Provider> getTripDeals() {
-		return tripDeals;
 	}
 
 }
