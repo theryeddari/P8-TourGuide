09839483bdeeae79b61b538b10e5de54231fa491
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 18:36:42 +0200

    use lombok Getter and Setter annotation instead of get method

--- TourGuide/src/main/java/com/openclassrooms/tourguide/helper/InternalTestHelper.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/helper/InternalTestHelper.java
@@ -1,15 +1,13 @@
 package com.openclassrooms.tourguide.helper;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class InternalTestHelper {
 
 	// Set this default up to 100,000 for testing
-	private static int internalUserNumber = 100;
-	
-	public static void setInternalUserNumber(int internalUserNumber) {
-		InternalTestHelper.internalUserNumber = internalUserNumber;
-	}
-	
-	public static int getInternalUserNumber() {
-		return internalUserNumber;
-	}
+	@Getter
+	@Setter
+    private static int internalUserNumber = 100;
+
 }
