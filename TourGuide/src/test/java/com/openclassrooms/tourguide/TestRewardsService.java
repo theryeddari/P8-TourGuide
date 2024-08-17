0ce9912ff7a21ca6034eca445aa327eb0c165fc0
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-17 17:07:56 +0200

    'assertTrue()' simplified to 'assertEquals()'

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
@@ -36,7 +36,7 @@ public class TestRewardsService {
 		tourGuideService.trackUserLocation(user);
 		List<UserReward> userRewards = user.getUserRewards();
 		tourGuideService.tracker.stopTracking();
-		assertTrue(userRewards.size() == 1);
+        assertEquals(1, userRewards.size());
 	}
 
 	@Test
