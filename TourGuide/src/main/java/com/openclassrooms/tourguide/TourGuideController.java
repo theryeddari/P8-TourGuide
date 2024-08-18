aefc17f092b9d9378f398f8fae1021d1c1a2d2d2
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-18 23:20:39 +0200

    adapt controller to respond with NearbyAttractionResponse and use getNearByAttractions()

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
@@ -2,6 +2,7 @@ package com.openclassrooms.tourguide;
 
 import java.util.List;
 
+import com.openclassrooms.tourguide.dto.NearbyAttractionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,18 +34,11 @@ public class TourGuideController {
     }
     
     //  TODO: Change this method to no longer return a List of Attractions.
- 	//  Instead: Get the closest five tourist attractions to the user - no matter how far away they are.
- 	//  Return a new JSON object that contains:
-    	// Name of Tourist attraction, 
-        // Tourist attractions lat/long, 
-        // The user's location lat/long, 
-        // The distance in miles between the user's location and each of the attractions.
         // The reward points for visiting each Attraction.
         //    Note: Attraction reward points can be gathered from RewardsCentral
     @RequestMapping("/getNearbyAttractions") 
-    public List<Attraction> getNearbyAttractions(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
-    	return tourGuideService.getNearByAttractions(visitedLocation);
+    public List<NearbyAttractionResponse> getNearbyAttractions(@RequestParam String userName) {
+    	return tourGuideService.getNearByAttractions(userName);
     }
     
     @RequestMapping("/getRewards") 
--- TourGuide/src/main/java/com/openclassrooms/tourguide/dto/InfoAboutNearbyAttractionDTO.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/dto/InfoAboutNearbyAttractionDTO.java
@@ -1,15 +1,19 @@
 package com.openclassrooms.tourguide.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class InfoAboutNearbyAttractionDTO {
     String attractionName;
-    Map<String,Long> attractionLocationXY;
-    Map<String,Long> userLocationXY;
+    List<Map<String,Double>> attractionLocationXY;
+    List<Map<String,Double>> userLocationXY;
+    double attractionDistance;
     int rewardPoints;
 }
